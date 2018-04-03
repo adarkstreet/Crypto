2a62d2dff8ed0b8840722e2364d0ae7da8ccd880
Andrew Street <andrew_street@live.com>
2018-04-03 11:47:58 -0600

    backend done

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -7,6 +7,14 @@ class Api::CoinsController < ApplicationController
   end
 
   def create
+    cmc_id = params[:coin].downcase
+    res = HTTParty.get("#{BASE_URL}#{cmc_id}")
+    if coin = Coin.create_by_cmc_id(res)
+      watched = WatchedCoin.find_or_create_by(coin_id: coin.id, user_id: current_user.id)
+      watched.update(initial_price: coin.price) if watched.initial_price.nil?
+    else
+      render json: { errors: 'Coin Not Found' }, status: 422
+    end
 
   end
 
